f747d8eb27592194e8fe3ddfed5f90143818a033
hyunwoo <kshsim98@naver.com>
2025-04-18 17:21:07 +0900

    requestMapping으로 변경

--- src/main/java/HS_hrs/vacation_service/Controller/HomeController.java
+++ src/main/java/HS_hrs/vacation_service/Controller/HomeController.java
@@ -1,12 +1,14 @@
 package HS_hrs.vacation_service.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/vacation")
 public class HomeController {
 
-    @GetMapping("/vacation")
+    @GetMapping
     public String home() {
         return "vacation 서비스입니다.";
     }
